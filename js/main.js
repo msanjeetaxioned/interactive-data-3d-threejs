7759b5f1c37fccf7f40b64ac22b65084828bebd0
msanjeetaxioned <sanjeetm@axioned.com>
2022-09-29 17:44:09 +0530

    Add animation for multiple bars

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ const calculateBarsHeightAndAddThemInScene = () => {
     const graph = [0.1, 10, 22.2, 48, 19.7];
     const barColors = [0xffd700, 0x8a2be2, 0x7fff00, 0x8b0000, 0x008080];
     const barMaxHeight = 15;
-    let xPos = -12;
+    let xPos = -14;
     let maxValue = graph[0];
     let barsHeight = [];
 
@@ -57,7 +57,7 @@ const calculateBarsHeightAndAddThemInScene = () => {
         bars[i].position.y = barsHeight[i]/2;
         bars[i].rotation.y = Math.PI / 4;
         scene.add(bars[i]);
-        xPos = xPos + 6;
+        xPos = xPos + 7;
     }
 }
 
@@ -79,62 +79,62 @@ let movementX = 0,
     timer2,
     rotations = [-360, -180, -90, 90, 180, 360];
 
-// const giveRotationDegreeAndSpeedAndDirection = () => {
-//     if (!rotation.isActive() && movementX != currentMovementX) {
-//         currentMovementX = movementX;
-//         if (currentMovementX < -25) {
-//             currentRotationSpeedAndDirection = rotations[0];
-//             // rotate bar -(360 + 30) degrees ie. -390 degrees
-//             rotation.to(cube.rotation, {y: "-=6.806784083", ease: "none", duration: 1})
-//             // rotate back the extra 30 degrees added previously for getting the effect on site
-//                 .to(cube.rotation, {y: "+=0.523598776", ease: "none", duration: 0.2});
-//         } else if (currentMovementX < -15) {
-//             currentRotationSpeedAndDirection = rotations[1];
-//             // rotate bar -(180 + 30) degrees ie. -210 degrees
-//             rotation.to(cube.rotation, {y: "-=3.66519143", ease: "none", duration: 0.75})
-//                 .to(cube.rotation, {y: "+=0.523598776", ease: "none", duration: 0.2});
-//         } else if (currentMovementX < 0) {
-//             if (currentRotationSpeedAndDirection == rotations[2]) {
-//                 if (!timer1) {
-//                     timer1 = setTimeout(() => {
-//                         clearTimeout(timer1);
-//                         // rotate bar -(90 + 30) degrees ie. -120 degrees
-//                         rotation.to(cube.rotation, {y: "-=2.094395103", ease: "none", duration: 0.5})
-//                             .to(cube.rotation, {y: "+=0.523598776", ease: "none", duration: 0.2});
-//                     }, 500);
-//                 }
-//             } else {
-//                 currentRotationSpeedAndDirection = rotations[2];
-//                 rotation.to(cube.rotation, {y: "-=2.094395103", ease: "none", duration: 0.5})
-//                     .to(cube.rotation, {y: "+=0.523598776", ease: "none", duration: 0.2});
-//             }
-//         } else if (currentMovementX == 0) {
-//             rotation.clear();
-//         } else if (currentMovementX <= 15) {
-//             if (currentRotationSpeedAndDirection == rotations[3]) {
-//                 if (!timer2) {
-//                     timer2 = setTimeout(() => {
-//                         clearTimeout(timer2);
-//                         rotation.to(cube.rotation, {y: "+=2.094395103", ease: "none", duration: 0.5})
-//                             .to(cube.rotation, {y: "-=0.523598776", ease: "none", duration: 0.2});
-//                     }, 500);
-//                 }
-//             } else {
-//                 currentRotationSpeedAndDirection = rotations[3];
-//                 rotation.to(cube.rotation, {y: "+=2.094395103", ease: "none", duration: 0.5})
-//                     .to(cube.rotation, {y: "-=0.523598776", ease: "none", duration: 0.2});
-//             }
-//         } else if (currentMovementX <= 25) {
-//             currentRotationSpeedAndDirection = rotations[4];
-//             rotation.to(cube.rotation, {y: "+=3.66519143", ease: "none", duration: 0.75})
-//                 .to(cube.rotation, {y: "-=0.523598776", ease: "none", duration: 0.2});
-//         } else {
-//             currentRotationSpeedAndDirection = rotations[5];
-//             rotation.to(cube.rotation, {y: "+=6.806784083", ease: "none", duration: 1})
-//                 .to(cube.rotation, {y: "-=0.523598776", ease: "none", duration: 0.2});
-//         }
-//     }
-// }
+const giveRotationDegreeAndSpeedAndDirection = (cube) => {
+    if (!rotation.isActive() && movementX != currentMovementX) {
+        currentMovementX = movementX;
+        if (currentMovementX < -25) {
+            currentRotationSpeedAndDirection = rotations[0];
+            // rotate bar -(360 + 30) degrees ie. -390 degrees
+            rotation.to(cube.rotation, {y: "-=6.806784083", ease: "none", duration: 1})
+            // rotate back the extra 30 degrees added previously for getting the effect on site
+                .to(cube.rotation, {y: "+=0.523598776", ease: "none", duration: 0.2});
+        } else if (currentMovementX < -15) {
+            currentRotationSpeedAndDirection = rotations[1];
+            // rotate bar -(180 + 30) degrees ie. -210 degrees
+            rotation.to(cube.rotation, {y: "-=3.66519143", ease: "none", duration: 0.75})
+                .to(cube.rotation, {y: "+=0.523598776", ease: "none", duration: 0.2});
+        } else if (currentMovementX < 0) {
+            if (currentRotationSpeedAndDirection == rotations[2]) {
+                if (!timer1) {
+                    timer1 = setTimeout(() => {
+                        clearTimeout(timer1);
+                        // rotate bar -(90 + 30) degrees ie. -120 degrees
+                        rotation.to(cube.rotation, {y: "-=2.094395103", ease: "none", duration: 0.5})
+                            .to(cube.rotation, {y: "+=0.523598776", ease: "none", duration: 0.2});
+                    }, 500);
+                }
+            } else {
+                currentRotationSpeedAndDirection = rotations[2];
+                rotation.to(cube.rotation, {y: "-=2.094395103", ease: "none", duration: 0.5})
+                    .to(cube.rotation, {y: "+=0.523598776", ease: "none", duration: 0.2});
+            }
+        } else if (currentMovementX == 0) {
+            rotation.clear();
+        } else if (currentMovementX <= 15) {
+            if (currentRotationSpeedAndDirection == rotations[3]) {
+                if (!timer2) {
+                    timer2 = setTimeout(() => {
+                        clearTimeout(timer2);
+                        rotation.to(cube.rotation, {y: "+=2.094395103", ease: "none", duration: 0.5})
+                            .to(cube.rotation, {y: "-=0.523598776", ease: "none", duration: 0.2});
+                    }, 500);
+                }
+            } else {
+                currentRotationSpeedAndDirection = rotations[3];
+                rotation.to(cube.rotation, {y: "+=2.094395103", ease: "none", duration: 0.5})
+                    .to(cube.rotation, {y: "-=0.523598776", ease: "none", duration: 0.2});
+            }
+        } else if (currentMovementX <= 25) {
+            currentRotationSpeedAndDirection = rotations[4];
+            rotation.to(cube.rotation, {y: "+=3.66519143", ease: "none", duration: 0.75})
+                .to(cube.rotation, {y: "-=0.523598776", ease: "none", duration: 0.2});
+        } else {
+            currentRotationSpeedAndDirection = rotations[5];
+            rotation.to(cube.rotation, {y: "+=6.806784083", ease: "none", duration: 1})
+                .to(cube.rotation, {y: "-=0.523598776", ease: "none", duration: 0.2});
+        }
+    }
+}
 
 function animate() {
 	requestAnimationFrame(animate);
@@ -143,8 +143,12 @@ function animate() {
 	// calculate objects intersecting the picking ray
 	var intersects = raycaster.intersectObjects(scene.children);
 
-	if (intersects.length > 0) {
-        // giveRotationDegreeAndSpeedAndDirection();
+	if (intersects.length == 2) {
+        for(let i = 0; i < bars.length; i++) {
+            if(bars[i].uuid == intersects[0].object.uuid) {
+                giveRotationDegreeAndSpeedAndDirection(bars[i]);
+            }
+        }
 	}
 	renderer.render(scene, camera);
 }
