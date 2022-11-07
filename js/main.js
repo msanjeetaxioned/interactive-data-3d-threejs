ed17ee65fcfbdb9168c5211dfe4a26ca3ee6b72a
msanjeetaxioned <sanjeetm@axioned.com>
2022-11-07 12:44:29 +0530

    Add function for calculating margin top for inbetween sections

--- js/main.js
+++ js/main.js
@@ -772,6 +772,20 @@ const appendGraphXValues = () => {
 	}
 }
 
+const calculateMinorSectionsMargins = () => {
+	const minorSections = document.querySelectorAll(".minor-section");
+	let margin;
+
+	if (document.body.clientWidth > 1330) {
+		margin = scale(document.body.clientWidth, 1330, 2560, -200, -600);
+	}
+
+	for (let i = 0; i < minorSections.length; i++) {
+		minorSections[i].style.marginTop = margin + "px";
+	}
+}
+calculateMinorSectionsMargins();
+
 const animate = () => {
 	requestAnimationFrame(animate);
 	graph1Raycaster.setFromCamera(graph1Mouse, graph1Camera);
